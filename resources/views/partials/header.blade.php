7fdc18fd45e9d595599a430bebda54ed34eda448
Difelice-e <difelice.emanuele@gmail.com>
2022-04-19 15:04:46 +0200

    gestito header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,62 @@
+<?php 
+$navLink = [
+    [
+        'id'=>'Characters',
+        'href'=>'#'
+    ],
+    [
+        'id'=>'Characters',
+        'href'=>'#'
+    ],
+    [
+        'id'=>'Characters',
+        'href'=>'#'
+    ],
+    [
+        'id'=>'Characters',
+        'href'=>'#'
+    ],
+    [
+        'id'=>'Characters',
+        'href'=>'#'
+    ],
+    [
+        'id'=>'Characters',
+        'href'=>'#'
+    ],
+    [
+        'id'=>'Characters',
+        'href'=>'#'
+    ],
+    [
+        'id'=>'Characters',
+        'href'=>'#'
+    ],
+    [
+        'id'=>'Characters',
+        'href'=>'#'
+    ],
+    [
+        'id'=>'Characters',
+        'href'=>'#'
+    ]
+];
+?>
+
 <header>
-    qui va l'header
+    <div class="container">
+        <picture class="logo">
+          <img src="/img/dc-logo.png" alt="logo" />
+        </picture>
+  
+        <nav class="main-nav">
+          <ul>
+            @foreach($navLink as $value)
+            <li class="nav__item">
+              <a href="{{ $value['href'] }}">{{ $value['id'] }}</a>
+            </li>
+            @endforeach
+          </ul>
+        </nav>
+      </div>
 </header>
--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
     @include('partials.header')
 
-    @yield('content')
-
+    <main>
+        @yield('content')
+    </main>
+    
     @include('partials.footer')
 </body>
 </html>
